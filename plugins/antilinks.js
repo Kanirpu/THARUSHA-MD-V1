0a34596d407eeea468ae6175adc6eefdd96f768e
Kanirpu <Kapoorni482@gmail.com>
2025-04-17 19:16:03 +0530

    Update antilinks.js

--- plugins/antilinks.js
+++ plugins/antilinks.js
@@ -14,7 +14,7 @@ cmd({
   sender
 }) => {
   try {
-    const badWords = ["wtf", "mia", "xxx", "fuck", 'sex', "huththa", "pakaya", 'ponnaya', "hutto"];
+    const badWords = ["wtf", "mia", "xxx", "fuck", 'sex', "huththa", "pakaya", 'ponnaya', "hutto", "Kariya"];
 
     if (!isGroup || isAdmins || !isBotAdmins) {
       return;
