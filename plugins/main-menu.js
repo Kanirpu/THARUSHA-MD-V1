3358b506dea00d79fec2fb19562d23185ce4ba21
Kanirpu <Kapoorni482@gmail.com>
2025-04-18 21:51:37 +0530

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -15,349 +15,308 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         let dec = `╭━━━〔 *${config.BOT_NAME}* 〕━━━┈⊷
-┃★╭──────────────
-┃★│ Owner : *${config.OWNER_NAME}*
-┃★│ Baileys : *Multi Device*
-┃★│ Type : *NodeJs*
-┃★│ Platform : *Heroku*
-┃★│ Mode : *[${config.MODE}]*
-┃★│ Prifix : *[${config.PREFIX}]*
-┃★│ Version : *3.0.0 Bᴇᴛᴀ*
-┃★╰──────────────
+┃★
+┃★ Owner : *${config.OWNER_NAME}*
+┃★ Baileys : *Multi Device*
+┃★ Type : *NodeJs*
+┃★ Platform : *Heroku*
+┃★ Mode : *[${config.MODE}]*
+┃★ Prifix : *[${config.PREFIX}]*
+┃★ Version : *3.0.0 Bᴇᴛᴀ*
 ╰━━━━━━━━━━━━━━━┈⊷
 
 ╭━━〔 *Download Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• facebook
-┃◈┃• mediafire
-┃◈┃• tiktok
-┃◈┃• twitter
-┃◈┃• Insta
-┃◈┃• apk
-┃◈┃• img
-┃◈┃• tt2
-┃◈┃• pins
-┃◈┃• apk2
-┃◈┃• fb2
-┃◈┃• pinterest 
-┃◈┃• spotify
-┃◈┃• play
-┃◈┃• play2
-┃◈┃• play3
-┃◈┃• play4
-┃◈┃• play5
-┃◈┃• play6
-┃◈┃• play7
-┃◈┃• play8
-┃◈┃• play9
-┃◈┃• play10
-┃◈┃• audio
-┃◈┃• video
-┃◈┃• video2
-┃◈┃• video3
-┃◈┃• video4
-┃◈┃• video5
-┃◈┃• video6
-┃◈┃• video7
-┃◈┃• video8
-┃◈┃• video9
-┃◈┃• video10
-┃◈┃• ytmp3
-┃◈┃• ytmp4
-┃◈┃• song
-┃◈┃• darama
-┃◈┃• gdrive
-┃◈┃• ssweb
-┃◈┃• tiks
-┃◈└───────────┈⊷
+┃◈
+┃◈ facebook
+┃◈ mediafire
+┃◈ tiktok
+┃◈ twitter
+┃◈ Insta
+┃◈ apk
+┃◈ img
+┃◈ tt2
+┃◈ pins
+┃◈ apk2
+┃◈ fb2
+┃◈ pinterest 
+┃◈ spotify
+┃◈ play
+┃◈ audio
+┃◈  video
+┃◈ ytmp3
+┃◈ ytmp4
+┃◈ song
+┃◈ darama
+┃◈ gdrive
+┃◈ ssweb
+┃◈ tiks
 ╰──────────────┈⊷
 
 ╭━━〔 *Group Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• grouplink
-┃◈┃• kickall
-┃◈┃• kickall2
-┃◈┃• kickall3
-┃◈┃• add
-┃◈┃• remove
-┃◈┃• kick
-┃◈┃• promote 
-┃◈┃• demote
-┃◈┃• dismiss 
-┃◈┃• revoke
-┃◈┃• setgoodbye
-┃◈┃• setwelcome
-┃◈┃• delete 
-┃◈┃• getpic
-┃◈┃• ginfo
-┃◈┃• delete 
-┃◈┃• disappear on
-┃◈┃• disappear off
-┃◈┃• disappear 7D,24H
-┃◈┃• allreq
-┃◈┃• updategname
-┃◈┃• updategdesc
-┃◈┃• joinrequests
-┃◈┃• senddm
-┃◈┃• nikal
-┃◈┃• mute
-┃◈┃• unmute
-┃◈┃• lockgc
-┃◈┃• unlockgc
-┃◈┃• invite
-┃◈┃• tag
-┃◈┃• hidetag
-┃◈┃• tagall
-┃◈┃• tagadmins
-┃◈└───────────┈⊷
+┃◈
+┃◈ grouplink
+┃◈ kickall
+┃◈ kickall2
+┃◈ kickall3
+┃◈ add
+┃◈ remove
+┃◈ kick
+┃◈ promote 
+┃◈ demote
+┃◈ dismiss 
+┃◈ revoke
+┃◈ setgoodbye
+┃◈ setwelcome
+┃◈ delete 
+┃◈ getpic
+┃◈ ginfo
+┃◈ delete 
+┃◈ disappear on
+┃◈ disappear off
+┃◈ disappear 7D,24H
+┃◈ allreq
+┃◈ updategname
+┃◈ updategdesc
+┃◈ joinrequests
+┃◈ senddm
+┃◈ nikal
+┃◈ mute
+┃◈ unmute
+┃◈ lockgc
+┃◈ unlockgc
+┃◈ invite
+┃◈ tag
+┃◈ hidetag
+┃◈ tagall
+┃◈ tagadmins
 ╰──────────────┈⊷
 
 ╭━━〔 *Reactions Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• bully @tag
-┃◈┃• cuddle @tag
-┃◈┃• cry @tag
-┃◈┃• hug @tag
-┃◈┃• awoo @tag
-┃◈┃• kiss @tag
-┃◈┃• lick @tag
-┃◈┃• pat @tag
-┃◈┃• smug @tag
-┃◈┃• bonk @tag
-┃◈┃• yeet @tag
-┃◈┃• blush @tag
-┃◈┃• smile @tag
-┃◈┃• wave @tag
-┃◈┃• highfive @tag
-┃◈┃• handhold @tag
-┃◈┃• nom @tag
-┃◈┃• bite @tag
-┃◈┃• glomp @tag
-┃◈┃• slap @tag
-┃◈┃• kill @tag
-┃◈┃• happy @tag
-┃◈┃• wink @tag
-┃◈┃• poke @tag
-┃◈┃• dance @tag
-┃◈┃• cringe @tag
-┃◈└───────────┈⊷
+┃◈
+┃◈ bully @tag
+┃◈ cuddle @tag
+┃◈ cry @tag
+┃◈ hug @tag
+┃◈ awoo @tag
+┃◈ kiss @tag
+┃◈ lick @tag
+┃◈ pat @tag
+┃◈ smug @tag
+┃◈ bonk @tag
+┃◈ yeet @tag
+┃◈ blush @tag
+┃◈ smile @tag
+┃◈ wave @tag
+┃◈ highfive @tag
+┃◈ handhold @tag
+┃◈ nom @tag
+┃◈ bite @tag
+┃◈ glomp @tag
+┃◈ slap @tag
+┃◈ kill @tag
+┃◈ happy @tag
+┃◈ wink @tag
+┃◈ poke @tag
+┃◈ dance @tag
+┃◈ cringe @tag
 ╰──────────────┈⊷
 
 ╭━━〔 *Logo List* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• neonlight
-┃◈┃• blackpink
-┃◈┃• dragonball
-┃◈┃• 3dcomic
-┃◈┃• america
-┃◈┃• naruto
-┃◈┃• sadgirl
-┃◈┃• clouds
-┃◈┃• futuristic
-┃◈┃• 3dpaper
-┃◈┃• eraser
-┃◈┃• sunset
-┃◈┃• leaf
-┃◈┃• galaxy
-┃◈┃• sans
-┃◈┃• boom
-┃◈┃• hacker
-┃◈┃• devilwings
-┃◈┃• nigeria
-┃◈┃• bulb
-┃◈┃• angelwings
-┃◈┃• zodiac
-┃◈┃• luxury
-┃◈┃• paint
-┃◈┃• frozen
-┃◈┃• castle
-┃◈┃• tatoo
-┃◈┃• valorant
-┃◈┃• bear
-┃◈┃• typography
-┃◈┃• birthday
-┃◈└───────────┈⊷
+┃◈
+┃◈ neonlight
+┃◈ blackpink
+┃◈ dragonball
+┃◈ 3dcomic
+┃◈ america
+┃◈ naruto
+┃◈ sadgirl
+┃◈ clouds
+┃◈ futuristic
+┃◈ 3dpaper
+┃◈ eraser
+┃◈ sunset
+┃◈ leaf
+┃◈ galaxy
+┃◈ sans
+┃◈ boom
+┃◈ hacker
+┃◈ devilwings
+┃◈ nigeria
+┃◈ bulb
+┃◈ angelwings
+┃◈ zodiac
+┃◈ luxury
+┃◈ paint
+┃◈ frozen
+┃◈ castle
+┃◈ tatoo
+┃◈ valorant
+┃◈ bear
+┃◈ typography
+┃◈ birthday
 ╰──────────────┈⊷
 
 ╭━━〔 *Owner Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• owner
-┃◈┃• menu
-┃◈┃• menu2
-┃◈┃• vv
-┃◈┃• listcmd
-┃◈┃• allmenu
-┃◈┃• repo
-┃◈┃• block
-┃◈┃• unblock
-┃◈┃• fullpp
-┃◈┃• setpp
-┃◈┃• restart
-┃◈┃• shutdown
-┃◈┃• updatecmd
-┃◈┃• alive
-┃◈┃• ping 
-┃◈┃• gjid
-┃◈┃• jid
-┃◈└───────────┈⊷
+┃◈
+┃◈ owner
+┃◈ menu
+┃◈ menu2
+┃◈ vv
+┃◈ listcmd
+┃◈ allmenu
+┃◈ repo
+┃◈ block
+┃◈ unblock
+┃◈ fullpp
+┃◈ setpp
+┃◈ restart
+┃◈ shutdown
+┃◈ updatecmd
+┃◈ alive
+┃◈ ping 
+┃◈ gjid
+┃◈ jid
 ╰──────────────┈⊷
 
 ╭━━〔 *Fun Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• shapar
-┃◈┃• rate
-┃◈┃• insult
-┃◈┃• hack
-┃◈┃• ship
-┃◈┃• character
-┃◈┃• pickup 
-┃◈┃• joke
-┃◈┃• hrt
-┃◈┃• hpy
-┃◈┃• syd
-┃◈┃• anger
-┃◈┃• shy
-┃◈┃• kiss
-┃◈┃• mon
-┃◈┃• cunfuzed
-┃◈┃• setpp
-┃◈┃• hand
-┃◈┃• nikal
-┃◈┃• hold
-┃◈┃• hug
-┃◈┃• nikal
-┃◈┃• hifi
-┃◈┃• poke
-┃◈└───────────┈⊷
+┃◈
+┃◈ shapar
+┃◈ rate
+┃◈ insult
+┃◈ hack
+┃◈ ship
+┃◈ character
+┃◈ pickup 
+┃◈ joke
+┃◈ hrt
+┃◈ hpy
+┃◈ syd
+┃◈ anger
+┃◈ shy
+┃◈ kiss
+┃◈ mon
+┃◈ cunfuzed
+┃◈ setpp
+┃◈ hand
+┃◈ nikal
+┃◈ hold
+┃◈ hug
+┃◈ nikal
+┃◈ hifi
+┃◈ poke
 ╰──────────────┈⊷
 
 ╭━━〔 *Convert Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• sticker
-┃◈┃• sticker2
-┃◈┃• emojimix
-┃◈┃• fancy
-┃◈┃• take
-┃◈┃• tomp3
-┃◈┃• tts
-┃◈┃• trt
-┃◈┃• base64
-┃◈┃• unbase64
-┃◈┃• binary
-┃◈┃• dbinary
-┃◈┃• tinyurl
-┃◈┃• urldecode
-┃◈┃• urlencode
-┃◈┃• url
-┃◈┃• repeat 
-┃◈┃• ask
-┃◈┃• readmore
-┃◈└───────────┈⊷
+┃◈
+┃◈ sticker
+┃◈ sticker2
+┃◈ emojimix
+┃◈ fancy
+┃◈ take
+┃◈ tomp3
+┃◈ tts
+┃◈ trt
+┃◈ base64
+┃◈ unbase64
+┃◈ binary
+┃◈ dbinary
+┃◈ tinyurl
+┃◈ urldecode
+┃◈ urlencode
+┃◈ url
+┃◈ repeat 
+┃◈ ask
+┃◈ readmore
 ╰──────────────┈⊷
 
 ╭━━〔 *Ai Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• ai
-┃◈┃• gpt3
-┃◈┃• gpt2
-┃◈┃• gptmini
-┃◈┃• gpt
-┃◈┃• meta
-┃◈┃• blackbox
-┃◈┃• luma
-┃◈┃• dj 
-┃◈┃• khan
-┃◈┃• jawad
-┃◈┃• gpt4
-┃◈┃• bing
-┃◈┃• imagine 
-┃◈┃• imagine2
-┃◈┃• copilot
-┃◈└───────────┈⊷
+┃◈
+┃◈ ai
+┃◈ gpt3
+┃◈ gpt2
+┃◈ gptmini
+┃◈ gpt
+┃◈ meta
+┃◈ blackbox
+┃◈ luma
+┃◈ dj 
+┃◈ khan
+┃◈ jawad
+┃◈ gpt4
+┃◈ bing
+┃◈ imagine 
+┃◈ imagine2
+┃◈ copilot
 ╰──────────────┈⊷
 
 ╭━━〔 *Main Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• ping
-┃◈┃• ping2
-┃◈┃• speed
-┃◈┃• live 
-┃◈┃• alive
-┃◈┃• runtime
-┃◈┃• uptime 
-┃◈┃• repo
-┃◈┃• owner
-┃◈┃• menu
-┃◈┃• menu2
-┃◈┃• restart
-┃◈└───────────┈⊷
+┃◈
+┃◈ ping
+┃◈ ping2
+┃◈ speed
+┃◈ live 
+┃◈ alive
+┃◈ runtime
+┃◈ uptime 
+┃◈ repo
+┃◈ owner
+┃◈ menu
+┃◈ menu2
+┃◈ restart
 ╰──────────────┈⊷
 
 ╭━━〔 *Anime Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• fack
-┃◈┃• truth
-┃◈┃• dare
-┃◈┃• dog
-┃◈┃• awoo
-┃◈┃• garl
-┃◈┃• waifu
-┃◈┃• neko
-┃◈┃• megnumin
-┃◈┃• neko
-┃◈┃• maid
-┃◈┃• loli
-┃◈┃• animegirl
-┃◈┃• animegirl
-┃◈┃• animegirl1
-┃◈┃• animegirl2
-┃◈┃• animegirl3
-┃◈┃• animegirl4
-┃◈┃• animegirl5
-┃◈┃• anime1
-┃◈┃• anime1
-┃◈┃• anime2
-┃◈┃• anime3
-┃◈┃• anime4
-┃◈┃• anime5
-┃◈┃• animenews
-┃◈┃• foxgirl
-┃◈┃• naruto
-┃◈└───────────┈⊷
+┃◈
+┃◈ fack
+┃◈ truth
+┃◈ dare
+┃◈ dog
+┃◈ awoo
+┃◈ garl
+┃◈ waifu
+┃◈ neko
+┃◈ megnumin
+┃◈ neko
+┃◈ maid
+┃◈ loli
+┃◈ animegirl
+┃◈ anime1
+┃◈ animenews
+┃◈ foxgirl
+┃◈ naruto
 ╰──────────────┈⊷
 
 ╭━━〔 *Other Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• timenow
-┃◈┃• date
-┃◈┃• count
-┃◈┃• calculate
-┃◈┃• countx
-┃◈┃• flip
-┃◈┃• coinflip
-┃◈┃• rcolor
-┃◈┃• roll
-┃◈┃• fact
-┃◈┃• cpp
-┃◈┃• rw
-┃◈┃• pair
-┃◈┃• pair2
-┃◈┃• pair3
-┃◈┃• fancy
-┃◈┃• logo <text>
-┃◈┃• define
-┃◈┃• news
-┃◈┃• movie
-┃◈┃• weather
-┃◈┃• srepo
-┃◈┃• insult
-┃◈┃• save
-┃◈┃• wikipedia
-┃◈┃• gpass
-┃◈┃• githubstalk
-┃◈┃• yts
-┃◈┃• ytv
-┃◈└───────────┈⊷
+┃◈
+┃◈ timenow
+┃◈ date
+┃◈ count
+┃◈ calculate
+┃◈ countx
+┃◈ flip
+┃◈ coinflip
+┃◈ rcolor
+┃◈ roll
+┃◈ fact
+┃◈ cpp
+┃◈ rw
+┃◈ pair
+┃◈ pair2
+┃◈ pair3
+┃◈ fancy
+┃◈ logo <text>
+┃◈ define
+┃◈ news
+┃◈ movie
+┃◈ weather
+┃◈ srepo
+┃◈ insult
+┃◈ save
+┃◈ wikipedia
+┃◈ gpass
+┃◈ githubstalk
+┃◈ yts
+┃◈ ytv
 ╰──────────────┈⊷
 ${config.DESCRIPTION}`;
 
